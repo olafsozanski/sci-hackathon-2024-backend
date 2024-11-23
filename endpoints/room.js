793a321a58ed4c8eaf72c3fc25c58bb1d04370fc
olafsozanski <olaf.sozanski@gmail.com>
2024-11-23 14:58:39 +0100

    some changes

--- endpoints/room.js
+++ endpoints/room.js
@@ -13,14 +13,14 @@ roomRouter.post('/', async (req, res) => {
 });
 
 roomRouter.get('/', async (req, res) => {
-    const room  = await Room.find({}).sort({name: 1});
+    const room  = await Room.find({}).sort({name: 1}).populate('responsiblePerson');
     res.json( room );
 });
 
 roomRouter.get('/:_id', async (req, res) => {
     const { _id } = req.params;
 
-    const room = await Room.findOne({ _id });
+    const room = await Room.findOne({ _id }).populate('responsiblePerson');
 
     if ( !room ) {
         return res.status(404).json({ message: 'Room not found' });
